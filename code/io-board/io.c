78518d76e098151fad1a4aaa88cbf463b1b95246
solderneer <sudhar393@gmail.com>
2017-07-03 19:53:55 +0800

    finish IO_Init function

--- code/io-board/io.c
+++ code/io-board/io.c
@@ -1,36 +1,47 @@
 #include "io.h"
 
-void IO_Write(uint8_t PORTx, uint8_t PINx, PinState_t pinState)
+void IO_Init(volatile uint8_t *PORTx, volatile uint8_t *DDRx, IO_InitStruct *IO_Init)
 {
-    
+    if(IO_Init->OutputSet == INPUT)
+    {
+        *DDRx &= ~(IO_Init->PinSet);
+
+        if(IO_Init->PullupSet == Pullup_Enable)
+            *PORTx |= IO_Init->PinSet;
+        else
+            *PORTx &= ~(IO_Init->PinSet);
+    }
+    else
+        *DDRx |= IO_Init->PinSet;
 }
 
-PinState_t IO_Read(uint8_t PORTx, uint8_t PINx)
+void IO_Write(volatile uint8_t *PORTx, uint8_t PINx, PinState_t pinState)
+{
+
+
+}
+
+PinState_t IO_Read(volatile uint8_t *PORTx, uint8_t PINx)
 {
     return SET;
 }
 
-void IO_Toggle(uint8_t PORTx, uint8_t PINx)
+void IO_Toggle(volatile uint8_t *PORTx, uint8_t PINx)
 {
-    
+
 }
 
-void IO_Set(uint8_t PORTx, uint8_t PINx)
+void IO_Set(volatile uint8_t *PORTx, uint8_t PINx)
 {
-    
+
 }
 
-void IO_Clear(uint8_t PORTx, uint8_t PINx)
+void IO_Clear(volatile uint8_t *PORTx, uint8_t PINx)
 {
-    
+
 }
 
-void IO_SetDir(uint8_t PORTx, uint8_t PINx)
+void IO_SetDir(volatile uint8_t *PORTx, uint8_t PINx)
 {
-    
-}
 
-void IO_Init(uint8_t PORTX, IO_InitStruct IO_Init)
-{
-    
 }
--- code/io-board/io.h
+++ code/io-board/io.h
@@ -13,13 +13,13 @@ typedef struct
 
 } IO_InitStruct;
 
-void IO_Write(uint8_t PORTx, uint8_t PINx, PinState_t);
-PinState_t IO_Read(uint8_t PORTx, uint8_t PINx);
-void IO_Toggle(uint8_t PORTx, uint8_t PINx);
-void IO_Set(uint8_t PORTx, uint8_t PINx);
-void IO_Clear(uint8_t PORTx, uint8_t PINx);
-void IO_SetDir(uint8_t PORTx, uint8_t PINx);
-void IO_Init(uint8_t PORTX, IO_InitStruct IO_Init);
+void IO_Write(volatile uint8_t *PORTx, uint8_t PINx, PinState_t);
+PinState_t IO_Read(volatile uint8_t *PORTx, uint8_t PINx);
+void IO_Toggle(volatile uint8_t *PORTx, uint8_t PINx);
+void IO_Set(volatile uint8_t *PORTx, uint8_t PINx);
+void IO_Clear(volatile uint8_t *PORTx, uint8_t PINx);
+void IO_SetDir(volatile uint8_t *PORTx, uint8_t PINx);
+void IO_Init(volatile uint8_t *PORTx, volatile uint8_t *DDRx, IO_InitStruct *IO_Init);
 
 
 
