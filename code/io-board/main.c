5d938f42d6b6e54e70ad2867afdb5fbb3c4f91ea
solderneer <sudhar393@gmail.com>
2017-07-05 08:33:14 +0800

    removed old code from main.c

--- code/io-board/main.c
+++ code/io-board/main.c
@@ -10,24 +10,5 @@ int extraTime = 0;
 
 int main(void)
 {
-  DDRC = 0xFF; //Nakes PORTC as Output
 
-  TCCR0A = (1 << COM0A1) | (1 << WGM01);
-  TCCR0B = (1 << CS02) | (1 << CS00);
-
-  TIMSK0 = (1 << OCIE0A);
-  sei();
-
-  while(1);
-}
-
-ISR(TIMER0_COMPA_vect)
-{
-  extraTime++;
-
-  if (extraTime > 100)
-  {
-    PORTC ^= 0xFF;
-    extraTime = 0;
-  }
 }
