fe4cc53a6e058457b2c4c70db9e59314397f2391
solderneer <sudhar393@gmail.com>
2017-07-10 12:38:27 +0800

    implemented driver adapter interface for i2c

--- code/io-board/i2c.h
+++ code/io-board/i2c.h
@@ -1,5 +1,14 @@
 #ifndef I2C_H_
 #define I2C_H_
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void I2C_Init(void);
+void I2C_WriteByte(uint8_t buffer);
+void I2C_ReadByte(uint8_t *buffer);
+void I2C_ioctl(void);
+void I2C_DeInit(void);
 
 #endif /* I2C_H_ */
